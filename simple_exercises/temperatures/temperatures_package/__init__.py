9c654650237cf4dd6298f40e91c2b331f0a09a1c
margareth91 <margerytka@wp.eu>
2023-10-26 22:09:31 +0200

    simple_exercises

---
+++ simple_exercises/temperatures/temperatures_package/__init__.py
@@ -0,0 +1,10 @@
+
+from generate import temperature_for_a_day, temperature_days
+from min_max_temp import min_max_temperature
+from warm_cold_days import warm_cold_days
+
+
+if __name__ == '__main__':
+    temperature_for_a_day()
+    min_max_temperature()
+    warm_cold_days()
